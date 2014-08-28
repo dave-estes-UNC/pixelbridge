ca704e3ea0f83f4ce3fc28fa0969291f90ebdd38
Dave Estes <cdestes@cs.unc.edu>
2014-08-27 21:23:57 -0400

    Adds (untested) multiplane support to existing ClCoefficientPlane calls.

--- src/ClCoefficientPlanes.h
+++ src/ClCoefficientPlanes.h
@@ -22,19 +22,21 @@ private:
 
     unsigned int      matrixWidth_;
     unsigned int      matrixHeight_;
-    unsigned int      matrixSize_;   // In bytes
+    unsigned int      matrixSize_;      // In bytes
+    unsigned int      coefficientSize_; // In bytes
     unsigned int      numPlanes_;
 
 #ifdef NARROW_DATA_STORES
-        int16_t             * coefficients_;
-        int16_t             * scalers_;
+    int16_t             * coefficients_;
+    int16_t             * scalers_;
 #else
-        int                 * coefficients_;
-        int                 * scalers_;
+    int                 * coefficients_;
+    int                 * scalers_;
 #endif
 
     inline unsigned int calcOffset(vector<unsigned int> &location) {
-        return (location[1] * width_ + location[0]) * matrixWidth_ * matrixHeight_;
+        assert(location.size() == 3);
+        return (location[2] * height_ * width_ + location[1] * width_ + location[0]) * matrixWidth_ * matrixHeight_;
     }
 
 public:
@@ -53,17 +55,18 @@ public:
         matrixWidth_ = matrixWidth;
         matrixHeight_ = matrixHeight;
 #ifdef NARROW_DATA_STORES
-        matrixSize_ = matrixWidth_ * matrixHeight_ * sizeof(int16_t);
+        coefficientSize_ = sizeof(int16_t);
 #else
-        matrixSize_ = matrixWidth_ * matrixHeight_ * sizeof(int);
+        coefficientSize_ = sizeof(int);
 #endif
+        matrixSize_ = matrixWidth_ * matrixHeight_ * coefficientSize_;
 
 #ifdef NARROW_DATA_STORES
-                coefficients_ = (int16_t *)malloc(CoefficientMatrix::memoryRequired(matrixWidth, matrixHeight) * displayWidth * displayHeight * numPlanes_);
-                scalers_ = (int16_t *)malloc(sizeof(int16_t) * 3 * displayWidth * displayHeight * numPlanes_);
+        coefficients_ = (int16_t *)malloc(CoefficientMatrix::memoryRequired(matrixWidth, matrixHeight) * displayWidth * displayHeight * numPlanes_);
+        scalers_ = (int16_t *)malloc(sizeof(int16_t) * 3 * displayWidth * displayHeight * numPlanes_);
 #else
-                coefficients_ = (int *)malloc(CoefficientMatrix::memoryRequired(matrixWidth, matrixHeight) * displayWidth * displayHeight * numPlanes_);
-                scalers_ = (int *)malloc(sizeof(int) * 3 * displayWidth * displayHeight * numPlanes_);
+        coefficients_ = (int *)malloc(CoefficientMatrix::memoryRequired(matrixWidth, matrixHeight) * displayWidth * displayHeight * numPlanes_);
+        scalers_ = (int *)malloc(sizeof(int) * 3 * displayWidth * displayHeight * numPlanes_);
 #endif
     }
 
@@ -79,10 +82,10 @@ public:
 
     void PutCoefficientMatrix(vector< vector<int> > &coefficientMatrix, vector<unsigned int> &location) {
 
-        // TODO(CDE): Add future support for multiple planes
-        //assert(location.size() == 2);
+        assert(location.size() == 3);
         assert(location[0] < width_);
         assert(location[1] < height_);
+        assert(location[2] < numPlanes_);
 
         unsigned int offset = calcOffset(location);
 #ifdef NARROW_DATA_STORES
@@ -115,29 +118,33 @@ public:
                                vector<unsigned int> &start,
                                vector<unsigned int> &end) {
 
-        // TODO(CDE): Add future support for multiple planes
-        //assert(start.size() == 2);
+        assert(start.size() == 3);
         assert(start[0] < width_);
         assert(start[1] < height_);
-        //assert(end.size() == 2);
+        assert(start[2] < numPlanes_);
+        assert(end.size() == 3);
         assert(end[0] < width_);
         assert(end[1] < height_);
+        assert(end[2] < numPlanes_);
 
         vector<unsigned int> position = start;
         unsigned int stride = end[0] - start[0] + 1;
         unsigned int rowCount = end[1] - start[1] + 1;
+        unsigned int planeCount = end[2] - start[2] + 1;
 
         // Copy matrices into coefficients_ and enqueue as one large write
         unsigned int offset;
-        for (position[1] = start[1]; position[1] <= end[1]; position[1]++) {
-            for (position[0] = start[0]; position[0] <= end[0]; position[0]++) {
-                offset = calcOffset(position);
-                for (int y = 0; y < matrixHeight_; y++) {
-                    for (int x = 0; x < matrixWidth_; x++) {
-                        if (coefficientMatrix[x][y] != COFFICIENT_UNCHANGED) {
-                            coefficients_[offset] = coefficientMatrix[x][y];
+        for (position[2] = start[2]; position[2] <= end[2]; position[2]++) {
+            for (position[1] = start[1]; position[1] <= end[1]; position[1]++) {
+                for (position[0] = start[0]; position[0] <= end[0]; position[0]++) {
+                    offset = calcOffset(position);
+                    for (int y = 0; y < matrixHeight_; y++) {
+                        for (int x = 0; x < matrixWidth_; x++) {
+                            if (coefficientMatrix[x][y] != COFFICIENT_UNCHANGED) {
+                                coefficients_[offset] = coefficientMatrix[x][y];
+                            }
+                            offset++;
                         }
-                        offset++;
                     }
                 }
             }
@@ -146,8 +153,8 @@ public:
         // Copy the rectangular region of coefficients to the compute device
         size_t row_pitch = matrixSize_ * width_;
         size_t slice_pitch = matrixSize_ * width_ * height_;
-        const size_t origin[3] = {start[0] * matrixSize_, start[1], 0};
-        const size_t region[3] = {(end[0] - start[0] + 1) * matrixSize_, end[1] - start[1] + 1, 1};
+        const size_t origin[3] = {start[0] * matrixSize_, start[1], start[2]};
+        const size_t region[3] = {(end[0] - start[0] + 1) * matrixSize_, end[1] - start[1] + 1, end[2] - start[2] + 1};
 
         int err = clEnqueueWriteBufferRect(clQueue_, clBuffer_, CL_FALSE,
                                            origin, origin, region,
@@ -158,8 +165,8 @@ public:
         }
 
         // Update Cost Model
-        costModel_->registerMemoryCharge(COEFFICIENT_PLANE_COMPONENT, READ_ACCESS, coefficients_ + calcOffset(start), matrixSize_* stride * rowCount, 0);
-        costModel_->registerMemoryCharge(COEFFICIENT_PLANE_COMPONENT, WRITE_ACCESS, coefficients_ + calcOffset(start), matrixSize_* stride * rowCount, 0);
+        costModel_->registerMemoryCharge(COEFFICIENT_PLANE_COMPONENT, READ_ACCESS, coefficients_ + calcOffset(start), matrixSize_* stride * rowCount * planeCount, 0);
+        costModel_->registerMemoryCharge(COEFFICIENT_PLANE_COMPONENT, WRITE_ACCESS, coefficients_ + calcOffset(start), matrixSize_* stride * rowCount * planeCount, 0);
     }
 
     void FillCoefficient(int coefficient,
@@ -167,8 +174,49 @@ public:
                          vector<unsigned int> &start,
                          vector<unsigned int> &end) {
 
-        // TODO(CDE): Implement #MultiPlaneCL
-        assert(false && "Needed for DCT Tiler.");
+        assert(start.size() == 3);
+        assert(start[0] < width_);
+        assert(start[1] < height_);
+        assert(start[2] < numPlanes_);
+        assert(end.size() == 3);
+        assert(end[0] < width_);
+        assert(end[1] < height_);
+        assert(end[2] < numPlanes_);
+
+        vector<unsigned int> position = start;
+        unsigned int stride = end[0] - start[0] + 1;
+        unsigned int rowCount = end[1] - start[1] + 1;
+        unsigned int planeCount = end[2] - start[2] + 1;
+
+        // Copy matrices into coefficients_ and enqueue as one large write
+        unsigned int offset;
+        for (position[2] = start[2]; position[2] <= end[2]; position[2]++) {
+            for (position[1] = start[1]; position[1] <= end[1]; position[1]++) {
+                for (position[0] = start[0]; position[0] <= end[0]; position[0]++) {
+                    offset = calcOffset(position);
+                    offset += row * matrixWidth_ + col;
+                    coefficients_[offset] = coefficient;
+                }
+            }
+        }
+
+        // Copy the rectangular region of coefficients to the compute device
+        size_t row_pitch = matrixSize_ * width_;
+        size_t slice_pitch = matrixSize_ * width_ * height_;
+        const size_t origin[3] = {start[0] * matrixSize_, start[1], start[2]};
+        const size_t region[3] = {(end[0] - start[0] + 1) * matrixSize_, end[1] - start[1] + 1, end[2] - start[2] + 1};
+
+        int err = clEnqueueWriteBufferRect(clQueue_, clBuffer_, CL_FALSE,
+                                           origin, origin, region,
+                                           row_pitch, slice_pitch, row_pitch, slice_pitch,
+                                           coefficients_, 0, NULL, NULL);
+        if (err != CL_SUCCESS) {
+            cout << __FUNCTION__ << " - Failed to create enqueue write buffer rect command." << endl;
+        }
+
+        // Update Cost Model
+        costModel_->registerMemoryCharge(COEFFICIENT_PLANE_COMPONENT, READ_ACCESS, coefficients_ + calcOffset(start), coefficientSize_* stride * rowCount * planeCount, 0);
+        costModel_->registerMemoryCharge(COEFFICIENT_PLANE_COMPONENT, WRITE_ACCESS, coefficients_ + calcOffset(start), coefficientSize_* stride * rowCount * planeCount, 0);
     }
 
     void FillScaler(Scaler scaler,
