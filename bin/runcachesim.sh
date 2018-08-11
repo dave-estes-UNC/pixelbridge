07bf6c43fab1b3a9e078941342735ab8e2545769
Dave Estes <cdestes@cs.unc.edu>
2018-08-11 19:58:30 -0400

    Working on experiment scripts for disseration. Update src/nddi.

--- bin/runcachesim.sh
+++ bin/runcachesim.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Runs all of the experiments. This includes the recordings, the videos
+# Runs all of the experiments while simulating cachec. This includes the recordings, the videos
 # with normal playback, and the videos with rewind playbayback
 
 if [ $# -ne 3 ]
 then
-	echo "Usage: runexperiments.sh <path-to-pixelbridge> <recordings-dir> <output-dirt>"
+	echo "Usage: runcachesim.sh <path-to-pixelbridge> <recordings-dir> <output-dirt>"
 	exit 1
 fi
 
@@ -14,25 +14,26 @@ REC_DIR=$2
 OUT_DIR=$3
 
 # For testing and exiting early
-#for x in `cat $REC_DIR/files`; do $PB --mode fb --start 200 --headless $REC_DIR/$x; done > $OUT_DIR/fb.log
-#for x in `cat $REC_DIR/videos`; do $PB --mode fb --start 300 --frames 800 --headless $REC_DIR/$x; done > $OUT_DIR/video-fb.log
-#for x in `cat $REC_DIR/videos`; do $PB --mode fb --start 300 --frames 1200 --rewind 800 200 --headless $REC_DIR/$x; done > $OUT_DIR/rewind-fb.log
-#exit;
+for x in `cat $REC_DIR/files`; do $PB --mode fb --start 200 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/fb.log
+#for x in `cat $REC_DIR/videos`; do $PB --mode fb --start 300 --frames 800 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/video-fb.log
+#for x in `cat $REC_DIR/videos`; do $PB --mode fb --start 300 --frames 1200 --rewind 800 200 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/rewind-fb.log
+for x in `ls costlog-*.json`; do gzip $x; mv $x.gz $OUT_DIR; done
+exit;
 
 # Framebuffer Mode
-for x in `cat $REC_DIR/files`; do $PB --mode fb --start 200 --headless $REC_DIR/$x; done > $OUT_DIR/fb.log
-for x in `cat $REC_DIR/videos`; do $PB --mode fb --start 300 --frames 800 --headless $REC_DIR/$x; done > $OUT_DIR/video-fb.log
-for x in `cat $REC_DIR/videos`; do $PB --mode fb --start 300 --frames 1200 --rewind 800 200 --headless $REC_DIR/$x; done > $OUT_DIR/rewind-fb.log
+for x in `cat $REC_DIR/files`; do $PB --mode fb --start 200 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/fb.log
+for x in `cat $REC_DIR/videos`; do $PB --mode fb --start 300 --frames 800 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/video-fb.log
+for x in `cat $REC_DIR/videos`; do $PB --mode fb --start 300 --frames 1200 --rewind 800 200 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/rewind-fb.log
 
 # Flat Tiled Mode
-for x in `cat $REC_DIR/files`; do $PB --mode flat --start 200 --headless $REC_DIR/$x; done > $OUT_DIR/flat.log
-for x in `cat $REC_DIR/videos`; do $PB --mode flat --start 300 --frames 800 --headless $REC_DIR/$x; done > $OUT_DIR/video-flat.log
-for x in `cat $REC_DIR/videos`; do $PB --mode flat --start 300 --frames 1200 --rewind 800 200 --headless $REC_DIR/$x; done > $OUT_DIR/rewind-flat.log
+for x in `cat $REC_DIR/files`; do $PB --mode flat --start 200 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/flat.log
+for x in `cat $REC_DIR/videos`; do $PB --mode flat --start 300 --frames 800 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/video-flat.log
+for x in `cat $REC_DIR/videos`; do $PB --mode flat --start 300 --frames 1200 --rewind 800 200 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/rewind-flat.log
 
 # Cached Tiled Mode (8)
-for x in `cat $REC_DIR/files`; do $PB --mode cache --tc 10000 --bits 8 --start 200 --headless $REC_DIR/$x; done > $OUT_DIR/cache8.log
-for x in `cat $REC_DIR/videos`; do $PB --mode cache --tc 10000 --bits 8 --start 300 --frames 800 --headless $REC_DIR/$x; done > $OUT_DIR/video-cache8.log
-for x in `cat $REC_DIR/videos`; do $PB --mode cache --tc 10000 --bits 8 --start 300 --frames 1200 --rewind 800 200 --headless $REC_DIR/$x; done > $OUT_DIR/rewind-cache8.log
+for x in `cat $REC_DIR/files`; do $PB --mode cache --tc 10000 --bits 8 --start 200 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/cache8.log
+for x in `cat $REC_DIR/videos`; do $PB --mode cache --tc 10000 --bits 8 --start 300 --frames 800 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/video-cache8.log
+for x in `cat $REC_DIR/videos`; do $PB --mode cache --tc 10000 --bits 8 --start 300 --frames 1200 --rewind 800 200 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/rewind-cache8.log
 
 # Cached Tiled Mode (4)
 for x in `cat $REC_DIR/files`; do $PB --mode cache --tc 10000 --bits 4 --psnr --start 200 $REC_DIR/$x; done > $OUT_DIR/cache4.log
@@ -60,9 +61,9 @@ for x in `cat $REC_DIR/videos`; do $PB --mode dct --quality 100 --psnr --start 3
 for x in `cat $REC_DIR/videos`; do $PB --mode dct --quality 100 --psnr --start 300 --frames 1200 --rewind 800 200 $REC_DIR/$x; done > $OUT_DIR/rewind-dct100.log
 
 # Count Mode (Perfect Pixel Latching)
-for x in `cat $REC_DIR/files`; do $PB --mode count --start 200 --headless $REC_DIR/$x; done > $OUT_DIR/count.log
-for x in `cat $REC_DIR/videos`; do $PB --mode count for--start 300 --frames 800 --headless $REC_DIR/$x; done > $OUT_DIR/video-count.log
-for x in `cat $REC_DIR/videos`; do $PB --mode count –start 300 --frames 1200 --rewind 800 200 --headless $REC_DIR/$x; done > $OUT_DIR/rewind-count.log
+for x in `cat $REC_DIR/files`; do $PB --mode count --start 200 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/count.log
+for x in `cat $REC_DIR/videos`; do $PB --mode count for--start 300 --frames 800 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/video-count.log
+for x in `cat $REC_DIR/videos`; do $PB --mode count –start 300 --frames 1200 --rewind 800 200 --logcosts fv $REC_DIR/$x; done > $OUT_DIR/rewind-count.log
 
 # Comment this out to run the blending experiments
 exit
