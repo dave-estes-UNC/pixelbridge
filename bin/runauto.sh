00cc00373f1ca6456b144f97e140fcd3b2ec0cdb
Dave Estes <dave.estes@gmail.com>
2019-08-14 16:40:30 -0400

    Adding the start and frame count to GroupCSV output for pred-res experiments.

--- bin/runauto.sh
+++ bin/runauto.sh
@@ -6,47 +6,47 @@ then
 	exit 1
 fi
 
-echo "GroupCSV,$4,1:8,$3,Snap,Delta"
+echo "GroupCSV,$4,$1,$2,1:8,$3,Snap,Delta"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 1:8 --dctsnap --dctdelta 0 --psnr --csv $4
-echo "GroupCSV,$4,1:8,$3,Trim,Delta"
+echo "GroupCSV,$4,$1,$2,1:8,$3,Trim,Delta"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 1:8 --dcttrim --dctdelta 0 --psnr --csv $4
-echo "GroupCSV,$4,1:8,$3,Snap,Planes"
+echo "GroupCSV,$4,$1,$2,1:8,$3,Snap,Planes"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 1:8 --dctsnap --dctplanes 0 --psnr --csv $4
-echo "GroupCSV,$4,1:8,$3,Trim,Planes"
+echo "GroupCSV,$4,$1,$2,1:8,$3,Trim,Planes"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 1:8 --dcttrim --dctplanes 0 --psnr --csv $4
 
-echo "GroupCSV,$4,16:1 1:8,$3,Snap,Delta"
+echo "GroupCSV,$4,$1,$2,16:1 1:8,$3,Snap,Delta"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 16:1,1:8 --dctsnap --dctdelta 0 --psnr --csv $4
-echo "GroupCSV,$4,16:1 1:8,$3,Trim,Delta"
+echo "GroupCSV,$4,$1,$2,16:1 1:8,$3,Trim,Delta"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 16:1,1:8 --dcttrim --dctdelta 0 --psnr --csv $4
-echo "GroupCSV,$4,16:1 1:8,$3,Snap,Planes"
+echo "GroupCSV,$4,$1,$2,16:1 1:8,$3,Snap,Planes"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 16:1,1:8 --dctsnap --dctplanes 0 --psnr --csv $4
-echo "GroupCSV,$4,16:1 1:8,$3,Trim,Planes"
+echo "GroupCSV,$4,$1,$2,16:1 1:8,$3,Trim,Planes"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 16:1,1:8 --dcttrim --dctplanes 0 --psnr --csv $4
 
-echo "GroupCSV,$4,2:2 1:8,$3,Snap,Delta"
+echo "GroupCSV,$4,$1,$2,2:2 1:8,$3,Snap,Delta"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 2:2,1:8 --dctsnap --dctdelta 0 --psnr --csv $4
-echo "GroupCSV,$4,2:2 1:8,$3,Trim,Delta"
+echo "GroupCSV,$4,$1,$2,2:2 1:8,$3,Trim,Delta"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 2:2,1:8 --dcttrim --dctdelta 0 --psnr --csv $4
-echo "GroupCSV,$4,2:2 1:8,$3,Snap,Planes"
+echo "GroupCSV,$4,$1,$2,2:2 1:8,$3,Snap,Planes"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 2:2,1:8 --dctsnap --dctplanes 0 --psnr --csv $4
-echo "GroupCSV,$4,2:2 1:8,$3,Trim,Planes"
+echo "GroupCSV,$4,$1,$2,2:2 1:8,$3,Trim,Planes"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 2:2,1:8 --dcttrim --dctplanes 0 --psnr --csv $4
 
-echo "GroupCSV,$4,4:3 1:8,$3,Snap,Delta"
+echo "GroupCSV,$4,$1,$2,4:3 1:8,$3,Snap,Delta"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 4:3,1:8 --dctsnap --dctdelta 0 --psnr --csv $4
-echo "GroupCSV,$4,4:3 1:8,$3,Trim,Delta"
+echo "GroupCSV,$4,$1,$2,4:3 1:8,$3,Trim,Delta"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 4:3,1:8 --dcttrim --dctdelta 0 --psnr --csv $4
-echo "GroupCSV,$4,4:3 1:8,$3,Snap,Planes"
+echo "GroupCSV,$4,$1,$2,4:3 1:8,$3,Snap,Planes"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 4:3,1:8 --dctsnap --dctplanes 0 --psnr --csv $4
-echo "GroupCSV,$4,4:3 1:8,$3,Trim,Planes"
+echo "GroupCSV,$4,$1,$2,4:3 1:8,$3,Trim,Planes"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 4:3,1:8 --dcttrim --dctplanes 0 --psnr --csv $4
 
-echo "GroupCSV,$4,8:2 4:2 1:8,$3,Snap,Delta"
+echo "GroupCSV,$4,$1,$2,8:2 4:2 1:8,$3,Snap,Delta"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 8:2,4:2,1:8 --dctsnap --dctdelta 0 --psnr --csv $4
-echo "GroupCSV,$4,8:2 4:2 1:8,$3,Trim,Delta"
+echo "GroupCSV,$4,$1,$2,8:2 4:2 1:8,$3,Trim,Delta"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 8:2,4:2,1:8 --dcttrim --dctdelta 0 --psnr --csv $4
-echo "GroupCSV,$4,8:2 4:2 1:8,$3,Snap,Planes"
+echo "GroupCSV,$4,$1,$2,8:2 4:2 1:8,$3,Snap,Planes"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 8:2,4:2,1:8 --dctsnap --dctplanes 0 --psnr --csv $4
-echo "GroupCSV,$4,8:2 4:2 1:8,$3,Trim,Planes"
+echo "GroupCSV,$4,$1,$2,8:2 4:2 1:8,$3,Trim,Planes"
 ./build/objdir-release/pixelbridge --start $1 --frames $2 --mode dct --dctbudget $3 --dctscales 8:2,4:2,1:8 --dcttrim --dctplanes 0 --psnr --csv $4
